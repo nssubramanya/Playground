5abe603e5a44c74fd5a70a2bcf9d6dae504b3f98
Subramanya Narasandra <subramanyans@Subramanyas-MacBook-Pro-3.local>
2019-04-13 10:02:51 +0530

    Hello Jenkins Project Added

---
+++ Jenkins/Projects/hello-jenkins/src/main/java/com/subbu/spring/hello_jenkins/App.java
@@ -0,0 +1,13 @@
+package com.subbu.spring.hello_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
